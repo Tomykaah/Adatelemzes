d2fd58ebf4ee18d38cdf8ebebd1ed912464f73ff
hagen <tamashagen212@gmail.com>
2025-12-04 16:51:03 +0100

    3/5

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ playerStatistics  = pd.read_csv('PlayerStatistics.csv', low_memory = False)
 teamStatistics    = pd.read_csv('TeamStatistics.csv', low_memory = False)
 teamHistories     = pd.read_csv('TeamHistories.csv', low_memory = False)
 
-##### 1. Ki volt az a játékos, aki a legtöbb pontot dobta
-#####     egy meccsen az NBA történelmében?
+##### 1. Ki volt az a játékos, aki a legtöbb pontot dobta egy meccsen az NBA történelmében?
 mostPoints = playerStatistics['points'].max()
 mostPointsPlayer = playerStatistics[playerStatistics['points'] == mostPoints]
 mostPointsName = mostPointsPlayer['firstName'] + ' ' + mostPointsPlayer['lastName']
@@ -17,3 +16,7 @@ print(mostPointsName)
 ##### 2. Melyik az a csapat, amelyik a legtöbb győzelmet érte el összesen?
 mostWinsTeam = teamStatistics.groupby('teamId')['win'].sum().idxmax()
 print('The team: ',teamHistories[teamHistories['teamId'] == mostWinsTeam][['teamCity', 'teamName']], ', max wins: ', teamStatistics.groupby('teamId')['win'].sum().max())
+
+##### 3. Ki az a játékos, aki a legidősebb volt, amikor utolsó mérkőzését játszotta?
+oldestPlayer = players.loc[players['dateOfBirth'].idxmin()]
+print(oldestPlayer['firstName'], oldestPlayer['lastName'], ' born on: ', oldestPlayer['dateOfBirth']) 
