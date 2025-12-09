d75fd5cf3b577c6a60fc2dab6924d03372aee674
hagen <tamashagen212@gmail.com>
2025-12-09 20:10:46 +0100

    asd

--- ownfunctions.py
+++ ownfunctions.py
@@ -1,6 +1,8 @@
-def convert_date_to_right_format(datestr):
-    from datetime import datetime as dt
+from datetime import datetime as dt
+
+def convert_ts_to_right_format(ts):
     try:
-        return dt.strftime(datestr, '%Y-%m-%d').date()
+        return dt.strptime(ts, '%Y-%m-%d %H:%M:%S').date()
     except:
-        return None
+        ts = ts[:-6]
+        return dt.strptime(ts, '%Y-%m-%d %H:%M:%S').date()
