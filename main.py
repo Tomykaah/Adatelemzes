9576123eb7a4f3b2d99a8be2bffe6a9646d3ca77
Tamas Hagen <tamashagen212@gmail.com>
2025-12-11 14:42:22 +0100

    final

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ mostPoints = playerStatistics['points'].max()
 mostPointsPlayer = playerStatistics[playerStatistics['points'] == mostPoints]
 mostPointsName = mostPointsPlayer['firstName'] + ' ' + mostPointsPlayer['lastName']
 
-print(mostPointsName)
+print('###1###')
+print('A legtöbb pontot dobó játékos: ', mostPointsName, ', pontszáma: ', mostPoints)
 
 ##### 2. Melyik az a csapat, amelyik a legtöbb győzelmet érte el összesen?
 mostWinsTeam = teamStatistics.groupby('teamId')['win'].sum().idxmax()
-
-print('The team: ',teamHistories[teamHistories['teamId'] == mostWinsTeam][['teamCity', 'teamName']], ', max wins: ', teamStatistics.groupby('teamId')['win'].sum().max())
+print('###2###')
+print('A legtöbb győzelmet elérő csapat: ', teamHistories[teamHistories['teamId'] == mostWinsTeam][['teamCity', 'teamName']].drop_duplicates(), ', győzelmeik száma: ', teamStatistics.groupby('teamId')['win'].sum().max())
 
 ##### 3. Ki az a játékos, aki a legidősebb volt, amikor utolsó mérkőzését játszotta?
 longestCareer = 0
@@ -48,6 +49,7 @@ for index, row in playersWithAge.iterrows():
 longestCareerPlayerFirstName = players[players['personId'] == longestCareerPlayer]['firstName'].values[0]
 longestCareerPlayerLastName = players[players['personId'] == longestCareerPlayer]['lastName'].values[0]
 
+print('###3###')
 print('A legidősebb játékos az utolsó mérkőzésén, amikor pályára lépett ', longestCareerPlayerFirstName,' ', longestCareerPlayerLastName, ' volt, összesen ', longestCareer, ' napig tartott karrierje.')
 
 ##### 4. Melyik csapatnak volt a legtöbb nemzetiségű játékosa egy szezonban?
@@ -59,6 +61,8 @@ teamPlayerList = teamPlayerList.drop_duplicates(subset = ['playerteamCity', 'pla
 maxNationalities = teamPlayerList.groupby(['playerteamCity', 'playerteamName', 'season'])['country'].count().max()
 
 maxNationalitiesTeam = teamPlayerList.groupby(['playerteamCity', 'playerteamName', 'season'])['country'].count().idxmax()
+
+print('###4###')
 print('A csapat: ', maxNationalitiesTeam, ', nemzetiségek száma: ', maxNationalities)
 
 ##### 5. Hogyan változott évről évre a legtöbbet átlagoló játékosok(pl. top25) pontszámainak átlaga?
